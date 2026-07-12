1fd9c464d592beb3d7c2cb0431ec2ecad3aaac9d
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:19:11 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,9 @@
-output "nat_gateway_public_ip_prefix_associations" {
-  description = "All nat_gateway_public_ip_prefix_association resources"
-  value       = azurerm_nat_gateway_public_ip_prefix_association.nat_gateway_public_ip_prefix_associations
-}
 output "nat_gateway_public_ip_prefix_associations_nat_gateway_id" {
-  description = "List of nat_gateway_id values across all nat_gateway_public_ip_prefix_associations"
-  value       = [for k, v in azurerm_nat_gateway_public_ip_prefix_association.nat_gateway_public_ip_prefix_associations : v.nat_gateway_id]
+  description = "Map of nat_gateway_id values across all nat_gateway_public_ip_prefix_associations, keyed the same as var.nat_gateway_public_ip_prefix_associations"
+  value       = { for k, v in azurerm_nat_gateway_public_ip_prefix_association.nat_gateway_public_ip_prefix_associations : k => v.nat_gateway_id }
 }
 output "nat_gateway_public_ip_prefix_associations_public_ip_prefix_id" {
-  description = "List of public_ip_prefix_id values across all nat_gateway_public_ip_prefix_associations"
-  value       = [for k, v in azurerm_nat_gateway_public_ip_prefix_association.nat_gateway_public_ip_prefix_associations : v.public_ip_prefix_id]
+  description = "Map of public_ip_prefix_id values across all nat_gateway_public_ip_prefix_associations, keyed the same as var.nat_gateway_public_ip_prefix_associations"
+  value       = { for k, v in azurerm_nat_gateway_public_ip_prefix_association.nat_gateway_public_ip_prefix_associations : k => v.public_ip_prefix_id }
 }
 
