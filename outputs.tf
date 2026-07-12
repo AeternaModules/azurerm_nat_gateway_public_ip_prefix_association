5c4c9f2f4332740ed4bcc6e66da447b1c19dd6f6
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:03:37 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "nat_gateway_public_ip_prefix_associations_id" {
+  description = "Map of id values across all nat_gateway_public_ip_prefix_associations, keyed the same as var.nat_gateway_public_ip_prefix_associations"
+  value       = { for k, v in azurerm_nat_gateway_public_ip_prefix_association.nat_gateway_public_ip_prefix_associations : k => v.id }
+}
 output "nat_gateway_public_ip_prefix_associations_nat_gateway_id" {
   description = "Map of nat_gateway_id values across all nat_gateway_public_ip_prefix_associations, keyed the same as var.nat_gateway_public_ip_prefix_associations"
   value       = { for k, v in azurerm_nat_gateway_public_ip_prefix_association.nat_gateway_public_ip_prefix_associations : k => v.nat_gateway_id }
